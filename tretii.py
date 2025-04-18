18a1e1bf3de40c9960274b220c5f500850d65590
Даниил <ccanga373@gmail.com>
2025-04-18 14:56:34 +0500

    Первый коммит

---
+++ tretii.py
@@ -0,0 +1,25 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import pytest
+from unittest.mock import patch
+from pervi import get_weather
+
+API_KEY = "62cd1b130ee3aa244b201f453665bce0"
+
+@patch("pervi.requests.get")
+def test_get_weather_city_not_found(mock_get):
+    mock_get.return_value.status_code = 404
+    mock_get.return_value.json.return_value = {}
+    result = get_weather("НеверныйГород", API_KEY)
+    assert "ошибка" in result
+    assert result["ошибка"] == "Город не найден"
+
+@patch("pervi.requests.get")
+def test_get_weather_invalid_key(mock_get):
+    mock_get.return_value.status_code = 401
+    mock_get.return_value.json.return_value = {}
+    result = get_weather("Almaty", "wrong_key")
+    assert "ошибка" in result
+    assert result["ошибка"] == "Неверный API-ключ"
